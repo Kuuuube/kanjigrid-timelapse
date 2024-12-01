e38cb9f314d6a3322267ffe5465079d25175033f
kuuuube <hexagonisalie@gmail.com>
2024-12-01 17:05:22 -0500

    Create output directory if it doesnt exist

--- generate.py
+++ generate.py
@@ -1,18 +1,23 @@
 import asyncio
 import pyppeteer
 import json
+import os
 
 async def main():
     browser = await pyppeteer.launch()
     page = await browser.newPage()
 
     json_path = input("Input JSON path: ")
-
     json_array = json.loads(open(json_path, "r").read())
+
+    output_directory = "images"
+
+    if not os.path.exists(output_directory):
+        os.makedirs(output_directory)
 
     for i, html_string in enumerate(json_array):
         await page.setContent(html_string)
-        await page.screenshot({'path': "images/" + str(i) + ".png", 'fullPage': True})
+        await page.screenshot({'path': output_directory + "/" + str(i) + ".png", 'fullPage': True})
 
     await browser.close()
 
