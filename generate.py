6aea4b990fdcfcb9ebedbfcaf2f8189fc2d9608c
kuuuube <hexagonisalie@gmail.com>
2024-12-01 15:39:43 -0500

    Add json array loading

--- generate.py
+++ generate.py
@@ -1,13 +1,16 @@
 import asyncio
 import pyppeteer
+import json
 
 async def main():
     browser = await pyppeteer.launch()
     page = await browser.newPage()
 
-    test_html_string = open("Crop_Theft_2024_12_01.html", "r").read()
-    await page.setContent(test_html_string)
-    await page.screenshot({'path': "full-page.png", 'fullPage': True})
+    json_array = json.loads(open("test_json.json", "r").read())
+
+    for i, html_string in enumerate(json_array):
+        await page.setContent(html_string)
+        await page.screenshot({'path': str(i) + ".png", 'fullPage': True})
 
     await browser.close()
 
